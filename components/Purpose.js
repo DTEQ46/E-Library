dde1baf3b7b2c6ef7285f072becc6b92b10222b1
DTEQ <olasehindedaniel109@gmail.com>
2022-02-14 20:51:08 -0500

    Another(2)

--- components/Purpose.js
+++ components/Purpose.js
@@ -3,21 +3,17 @@ import Link from "next/link";
 function Purpose() {
   return (
     <div className={styles.container}>
-      <div>
-        <p className={styles.p1}>
-          Our priority is to help both secondary school students and teachers
-          get easy access to relevant books for learning...
-        </p>
-      </div>
-      <div>
-        <p className={styles.p2}>
-          Click{" "}
-          <Link href="#">
-            <a>here </a>
-          </Link>
-          to learn more about our library’s mode of operation
-        </p>
-      </div>
+      <p className={styles.p1}>
+        Our priority is to help both secondary school students and teachers get
+        easy access to relevant books for learning...
+      </p>
+      <p className={styles.p2}>
+        Click{" "}
+        <Link href="#">
+          <a>here </a>
+        </Link>
+        to learn more about our library’s mode of operation
+      </p>
     </div>
   );
 }
